3e79efd8b3f11a155cfb8fe8a614f8d31453e2e9
Andrew Barker <barkand@uw.edu>
2020-02-21 13:17:13 -0800

    fixing issues from trying to commit the env

--- pytorch_roberta.sh
+++ pytorch_roberta.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+source ~/.bashrc
+conda activate the_count_env
+
 python3 ./pytorch_roberta.py -data ./data/ja_syn_pair_words.txt -labels ./data/ja_syn_labels.txt -v 1 -epochs 5 > roberta_output
