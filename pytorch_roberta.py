360cc556cb71bc08357e15151e941e5661423470
Andrew B <barkand@uw.edu>
2020-02-28 12:00:20 -0800

    asdf;lafds

--- pytorch_roberta.py
+++ pytorch_roberta.py
@@ -99,7 +99,7 @@ def get_model(model_name):
         # config = BertConfig.from_pretrained('bert-base-multilingual-cased')
         model = BertForSequenceClassification.from_pretrained('bert-base-multilingual-cased')
 
-    config.num_labels = 2
+    # config.num_labels = 2
 
     return model
 
