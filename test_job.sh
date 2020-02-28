2d77e2f21bc7f7de80514418c4e8773874127cda
Andrew B <barkand@uw.edu>
2020-02-28 12:07:05 -0800

    added test job

---
+++ test_job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ~/.bashrc
+conda activate the_count_env
+
+python ./pytorch_roberta.py -task sem -epochs 1 -model roberta -train ./data/en_sem_train.csv -test ./data/en_sem_test.csv -mb 32
