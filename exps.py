f71786a3cf5c3f8fb1696ca2deee0604f805034f
Andrew B <barkand@uw.edu>
2020-02-27 17:25:38 -0800

    plz work

--- exps.py
+++ exps.py
@@ -7,10 +7,7 @@ tasks = ['syn', 'sem']
 for task in tasks:
     for model in models:
         for lang in langs:
-            print('Starting Training: \n\
-                Task: {}, Model: {}, Lang: {}'.format(task, model, lang))
-            result = subprocess.check_output('python3 ./pytorch_roberta.py -v 100 \
-                -train {}_{}_train.csv \
-                -test {}_{}_test.csv -model {} -epochs 20'.format(lang, task, lang, task, model), shell=True)
+            print('Starting Training: \nTask: {}, Model: {}, Lang: {}'.format(task.upper(), model.upper(), lang.upper()))
+            result = subprocess.check_output('python3 ./pytorch_roberta.py -v 100 -train {}_{}_train.csv -test {}_{}_test.csv -model {} -epochs 20'.format(lang, task, lang, task, model), shell=True)
             with open('./results/{}_{}_{}.txt'.format(task, model, lang), 'w+') as f:
                 f.write(result)
