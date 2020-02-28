6ab694ec9f7743ef9bb632f0ca6b3c36f70ce845
Andrew B <barkand@uw.edu>
2020-02-27 16:55:55 -0800

    finished script

--- syn_exps.py
+++ syn_exps.py
@@ -7,6 +7,7 @@ tasks = ['syn', 'sem']
 for lang in langs:
     for model in models:
         for task in tasks:
+            print('Starting {} {} {}'.format(lang, task, model))
             result = subprocess.check_output('python3 ./pytorch_roberta.py -v 100 \
                 -train {}_{}_train.csv \
                 -test {}_{}_test.csv -model {} -epochs 20'.format(lang, task, lang, task, model), shell=True)
