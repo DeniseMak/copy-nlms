ad8694b5e82795e6cff2fd873afa48adbaa833dc
Andrew B <barkand@uw.edu>
2020-02-27 16:47:18 -0800

    added shell script for condor

---
+++ condor_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source ~/.bashrc
+conda activate the_count_env
+
+python3 ./pytorch_roberta.py -data ./data/en_syn_pair_words.txt -labels ./data/en_syn_labels.txt -v 100 -epochs 25 > roberta_output_syn
+
+python3 ./pytorch_roberta.py -data ./data/en_sem_pair_words.txt -labels ./data/en_sem_labels.txt -v 100 -epochs 25 > roberta_output_sem
