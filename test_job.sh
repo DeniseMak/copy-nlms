fa81af15353b5511c5b932137ec8a912d1cdf7c0
Andrew B <barkand@uw.edu>
2020-02-28 12:30:54 -0800

    changed verbosity

--- test_job.sh
+++ test_job.sh
@@ -3,4 +3,4 @@
 source ~/.bashrc
 conda activate the_count_env
 
-python ./pytorch_roberta.py -task sem -epochs 1 -model roberta -train ./data/en_sem_train.csv -test ./data/en_sem_test.csv -mb 32 > test.txt
+python ./pytorch_roberta.py -task sem -epochs 1 -model roberta -train ./data/en_sem_train.csv -test ./data/en_sem_test.csv -mb 32 -v 1 > test.txt
