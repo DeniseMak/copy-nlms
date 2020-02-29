8e29bd59153f853e7b07c87739e2a1feaf5d4a2a
Andrew B <barkand@uw.edu>
2020-02-28 17:59:07 -0800

    changed verbosity

--- exps.py
+++ exps.py
@@ -2,12 +2,12 @@ import os
 
 langs = ['en', 'ja', 'dk', 'fr']
 models = ['roberta', 'bert', 'xlm']
-tasks = ['sem']#, 'sem']
+tasks = ['sem', 'syn']
 
 for task in tasks:
     for model in models:
         for lang in langs:
             print('Starting Training: \nTask: {}, Model: {}, Lang: {}'.format(task.upper(), model.upper(), lang.upper()))
-            os.system('python3 ./models.py -v 100 -train ./data/{}_{}_train.csv -test ./data/{}_{}_test.csv -model {} -epochs 1 -out_f ./results/{}_{}_{}.txt'.format(lang, task, lang, task, model, lang, task, model))
+            os.system('python3 ./models.py -v 1 -train ./data/{}_{}_train.csv -test ./data/{}_{}_test.csv -model {} -epochs 1 -out_f ./results/{}_{}_{}.txt'.format(lang, task, lang, task, model, lang, task, model))
             # with open('./results/{}_{}_{}.txt'.format(task, model, lang), 'w+') as f:
             #     f.write(result)
