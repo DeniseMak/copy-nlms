2e49e0c4780e4da3b3c53941509df765b1fc5287
Andrew B <barkand@uw.edu>
2020-02-28 21:50:09 -0800

    updated data and made larger minibatches

--- data_generation/gen_all_data.py
+++ data_generation/gen_all_data.py
@@ -6,4 +6,4 @@ tasks = ['syn', 'sem']
 for task in tasks:
     for lang in langs:
         print('Generating: \nTask: {}, Lang: {}'.format(task.upper(), lang.upper()))
-        os.system('python3 ./gen_{}_data.py -lang {} -samples 5000 -range 10000'.format(task, lang))
+        os.system('python3 ./gen_{}_data.py -lang {} -samples 10000 -range 5000'.format(task, lang))
--- exps.py
+++ exps.py
@@ -8,6 +8,4 @@ for task in tasks:
     for model in models:
         for lang in langs:
             print('Starting Training: \nTask: {}, Model: {}, Lang: {}'.format(task.upper(), model.upper(), lang.upper()))
-            os.system('python3 ./models.py -v 1 -train ./data/{}_{}_train.csv -test ./data/{}_{}_test.csv -model {} -epochs 1 -out_f ./results/{}_{}_{}.txt'.format(lang, task, lang, task, model, lang, task, model))
-            # with open('./results/{}_{}_{}.txt'.format(task, model, lang), 'w+') as f:
-            #     f.write(result)
+            os.system('python3 ./models.py -v 1 -train ./data/{}_{}_train.csv -test ./data/{}_{}_test.csv -model {} -epochs 1 -mb 256 -out_f ./results/{}_{}_{}.txt'.format(lang, task, lang, task, model, lang, task, model))
