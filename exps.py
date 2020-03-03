516bebb14a5eb115128eb412e3a756e7907cfe26
Andrew B <barkand@uw.edu>
2020-03-02 19:52:58 -0800

    plz work

--- exps.py
+++ exps.py
@@ -6,5 +6,7 @@ models = ['xlm', 'bert']
 for task in tasks:
     for model in models:
         for lang in langs:
-            print('Starting Training: \nTask: {}, Model: {}, Lang: {}'.format(task.upper(), model.upper(), lang.upper()))
+            with open('test', 'w+') as f:
+                f.write('Starting Training: \nTask: {}, Model: {}, Lang: {}'.format(task.upper(), model.upper(), lang.upper()))
+            # print('Starting Training: \nTask: {}, Model: {}, Lang: {}'.format(task.upper(), model.upper(), lang.upper()))
             os.system('python3 ./models.py -v 1 -train ./data/{}_{}_train.csv -test ./data/{}_{}_test.csv -model {} -epochs 10 -mb 256 -out_f ./results/{}_{}_{}.txt'.format(lang, task, lang, task, model, lang, task, model))
