be561dc5a7c2560e347e26d7e7b2282d933d8df2
Andrew B <barkand@uw.edu>
2020-03-07 15:50:10 -0800

    trying to deal with memory issues

--- models.py
+++ models.py
@@ -201,7 +201,7 @@ def evaluate_data(data, model, path):
     # all_true = list()
         for sents, x, y in data:
             sents = list(sents)
-            predicted = get_preds(x, y, model).tolist()
+            predicted = get_preds(x, y, model)[0].tolist()
             # all_preds += list(predicted)
             y = y.tolist()
 
