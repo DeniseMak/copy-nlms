525b2fc7ac5906c0d74468e4ff59c409d0e5a656
Andrew B <barkand@uw.edu>
2020-03-07 16:57:10 -0800

    reducing mb size

--- exps.py
+++ exps.py
@@ -10,4 +10,4 @@ for task in tasks:
     for model in models:
         for lang in langs:
             print('Starting Training: \nTask: {}, Model: {}, Lang: {}'.format(task.upper(), model.upper(), lang.upper()))
-            os.system('python3 ./models.py -v 100 -train ./data/{}_{}_train.csv -test ./data/{}_{}_test.csv -model {} -epochs 100 -mb 64 -out_f ./results/{}_{}_{}.txt'.format(lang, task, lang, task, model, lang, task, model))
+            os.system('python3 ./models.py -v 100 -train ./data/{}_{}_train.csv -test ./data/{}_{}_test.csv -model {} -epochs 100 -mb 32 -out_f ./results/{}_{}_{}.txt'.format(lang, task, lang, task, model, lang, task, model))
